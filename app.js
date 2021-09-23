ff3e2daae8d046e556df535a889936f3616a33fe
Oleg <oleg17.k@gmail.com>
2021-09-23 21:30:33 +0300

    added some new measures, styled

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ fetch(
     console.log(data);
     let city = document.querySelector(".city");
     city.innerHTML = "Поточне розташування: " + data.name;
-    // let date = document.querySelector(".date");
-    // date.innerHTML = data.dt;
+
     let temp = document.querySelector(".temp");
     temp.innerHTML = "Температура: " + data.main.temp + " &#176C";
+    let tempFeelsLike = document.querySelector(".temp-feels-like");
+    tempFeelsLike.innerHTML =
+      "Відчувається як: " + data.main.feels_like + " &#176C";
     let pressure = document.querySelector(".pressure");
     pressure.innerHTML = "Тиск: " + data.main.pressure + " hPa";
     let description = document.querySelector(".description");
     description.innerHTML = "Опис: " + data.weather[0].description;
     let humidity = document.querySelector(".humidity");
-    humidity.innerHTML = "Вологсть: " + data.main.humidity + "%";
+    humidity.innerHTML = "Вологість: " + data.main.humidity + "%";
     let speed = document.querySelector(".speed");
     speed.innerHTML = "Швидкість вітру: " + data.wind.speed + " м/с";
     let deg = document.querySelector(".deg");
@@ -24,4 +26,14 @@ fetch(
     let iconNow = data.weather[0].icon;
     icon.innerHTML =
       '<img src="https://openweathermap.org/img/w/' + iconNow + '.png">';
+
+    let unixTimestamp = data.dt;
+
+    let milliseconds = unixTimestamp * 1000;
+
+    let dateObject = new Date(milliseconds);
+
+    let humanDateFormat = document.querySelector(".date");
+    humanDateFormat.innerHTML =
+      "Погода станом на: " + dateObject.toLocaleString();
   });
